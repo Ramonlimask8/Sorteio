bb2b231cd2d673f8c8497299ec50c863268ee8de
ramon.flsilva1 <ramon.flsilva1@CAS555601W0745.senacsp.edu.br>
2017-03-23 21:59:18 -0300

    Sorteio usando o Random

---
+++ src/sorteio/Sorteio.java
@@ -0,0 +1,19 @@
+
+package sorteio;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class Sorteio {
+
+    
+    public static void main(String[] args) {
+      Scanner console = new Scanner (System.in);
+      Random rng = new Random();
+      
+      int i = rng.nextInt(10);
+              
+              System.out.println(i);
+    }
+    
+}
